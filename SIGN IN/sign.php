5d077785bfd47bef16fc7a6f40f23603cfbd21c1
Wilson Goal <151470917+Goal651@users.noreply.github.com>
2024-03-22 21:49:22 +0200

    update

--- SIGN IN/sign.php
+++ SIGN IN/sign.php
@@ -1,15 +1,24 @@
 <?php
-$host="localhost";
-$user="root";
-$pass="";
-$db="financial_data";
-$conn=new mysqli($host,$user,$pass,$db);
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'financial_data';
+$conn = new mysqli( $host, $user, $pass, $db );
 $conn->connect;
-if($conn->coonect_error){
-    die("Connection failed".$conn->connect_error);
+if ( $conn->coonect_error ) {
+    die( 'Connection failed'.$conn->connect_error );
+} else {
+    $email = $_POST[ 'email' ];
+    $username = $_POST[ 'username' ];
+    $password = $_POST[ 'password' ];
+
+    $method = $_SERVER[ 'method' ];
+    if ( $method == 'post' ) {
+        $conn->query( "insert into users (username,email,password) values(`$username`,`$email`,`$password`) " );
+        echo json_encode( array( 'status'=>'success' ) );
+
+    }
 }
-else{
-    echo json_encode(array("status"=>"success"));
-}
+
 $conn->close;
 ?>
