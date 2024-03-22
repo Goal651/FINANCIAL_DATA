1112ccab224e485dec76edf6ed7fbf51572fe601
Wilson Goal <151470917+Goal651@users.noreply.github.com>
2024-03-22 20:44:50 +0200

    update index

--- LOGIN/login.js
+++ LOGIN/login.js
@@ -7,19 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function addStudent() {
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                
-            } else {
-                console.error('Failed to add student.');
-            }
-        }
-    };
-    xhr.open('POST', 'login.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send('email=' + email + '&password=' + password );
+    let email = $('#email').val();
+    let password = $('#password').val();
+    $.ajax({
+        type:"post",
+        url:"/save",
+        dataType
+    })
+   
 }
